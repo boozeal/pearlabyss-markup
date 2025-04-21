19e8a0d5fe22e88de511ebcb94b7f843de37205a
김성현 <skyladder8895@gmail.com>
2025-04-21 14:36:19 +0900

    fix: change page param's type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,11 @@ import { redirect } from "next/navigation";
 import { fallbackLng, languages } from "@/i18n/settings";
 
 export default async function Page({
-  params: { lng },
+  params,
 }: {
-  params: { lng: string };
+  params: Promise<{ lng: string }>;
 }) {
+  let { lng } = await params;
   if (languages.indexOf(lng) < 0) lng = fallbackLng;
   redirect(`/${lng}/main`);
 }
